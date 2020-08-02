2e7d744f8edefc60c45d1e89e7c695a78b8b797c
Akshara Sundararajan <49100882+akshara-sun@users.noreply.github.com>
2020-08-01 18:06:03 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -90,30 +90,60 @@ export interface SoundCollection extends Collection<Sound> {
  * @ignore
  */
 interface RawSound {
+  //Sound ID on Freesound
   id: number,
-  url: string,
+  //The URl for this sound on the Freesound website
+  url: string, 
+  //The name user gave to the sound
   name: string,
+  //An array of tags the user gave to the sound 
   tags: string[],
+  //The description the user gave to the sound.
   description: string,
+  /*Latitude and longitude of the geotag separated by spaces (e.g. “41.0082325664 28.9731252193”, 
+  only for sounds that have been geotagged)*/
   geotag: null,
+  //The date when the sound was uploaded (e.g. “2014-04-16T20:07:11.145”)
   created: string,
+  //The license under which the sound is available to you
   license: string,
+  //The type of sound (wav, aif, aiff, mp3, m4a or flac)
   type: string,
+  //The number of channels
   channels: number,
+  //The size of the file in bytes
   filesize: number,
+  //The bit rate of the sound in kbps
   bitrate: number,
+  //The bit depth of the sound
   bitdepth: number,
+  //The duration of the sound in seconds
   duration: number,
+  //The samplerate of the sound
   samplerate: number,
+  //The username of the uploader of the sound
   username: string,
+  //If the sound is part of a pack, this URl points to that pack’s API resource
   pack: string,
+  //
   pack_name?: string,
+  /*
+  Dictionary containing the URIs for mp3 and ogg versions of the sound. 
+  The dictionary includes the fields preview-hq-mp3 and preview-lq-mp3 (for ~128kbps quality and ~64kbps quality mp3 respectively), 
+  and preview-hq-ogg and preview-lq-ogg (for ~192kbps quality and ~80kbps quality ogg respectively). 
+  API authentication is required for retrieving sound previews (Token or OAuth2)
+  */
   previews: {
     'preview-lq-ogg': string,
     'preview-lq-mp3': string,
     'preview-hq-ogg': string,
     'preview-hq-mp3': string
   },
+  /*
+  Dictionary including the URls for spectrogram and waveform visualizations of the sound. 
+  The dictionary includes the fields waveform_l and waveform_m (for large and medium waveform images respectively), 
+  and spectral_l and spectral_m (for large and medium spectrogram images respectively).
+  */
   images: {
     spectral_m: string,
     spectral_l: string,
@@ -124,15 +154,31 @@ interface RawSound {
     waveform_m: string,
     spectral_bw_m: string
   },
+  //The number of times the sound was downloaded.
   num_downloads: number,
+  //The average rating of the sound.
   avg_rating: number,
+  //The number of times the sound was rated.
   num_ratings: number,
+  //The URl of a paginated list of the comments of the sound.
   comments: string,
+  //The number of comments.
   num_comments: number,
+  //URI pointing to the similarity resource (to get a list of similar sounds).
   similar_sounds: string,
+  /*
+  Dictionary containing requested descriptors information according to the descriptors request parameter (see below). 
+  This field will be null if no descriptors were specified (or invalid descriptor names specified) or if the analysis data 
+  for the sound is not available.
+  */
   analysis: string,
+  /*The URl for retrieving a JSON file with analysis information for each frame of the sound 
+  (see Analysis Descriptor Documentation)
+  */
   analysis_frames: string,
+  //URl pointing to the complete analysis results of the sound (see Analysis Descriptor Documentation).
   analysis_stats: string,
+  //Dictionary containing the results of the AudioCommons analysis for the given sound.
   ac_analysis: {
     ac_tempo_confidence: number,
     ac_note_confidence: number,
